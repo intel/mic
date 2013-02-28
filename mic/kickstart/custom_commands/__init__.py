20ed61cfc0cefb20dda740bf449a940b3f294711
Huang Hao <hao.h.huang@intel.com>
2013-02-28 14:06:02 +0800

    Fix import error of kickstart.custom_commands
    Names listed in __all__ don't exist.
    
    >>> from mic.kickstart.custom_commands import *
    Traceback (most recent call last):
      File "<stdin>", line 1, in <module>
    AttributeError: 'module' object has no attribute 'Mic_Desktop'
    
    Change-Id: Ie9ea1cccacb5fb9faaabc1951c513425c377d549

--- mic/kickstart/custom_commands/__init__.py
+++ mic/kickstart/custom_commands/__init__.py
@@ -1,6 +1,7 @@
-import desktop
-import micrepo
-import partition
+from desktop import Mic_Desktop
+from micrepo import Mic_Repo, Mic_RepoData
+from partition import Mic_Partition
+
 
 __all__ = (
     "Mic_Desktop",
