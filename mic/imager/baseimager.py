31ee84cad6ab8a06ec57709a31f06b1423be82ad
Gui Chen <gui.chen@intel.com>
2013-03-24 14:58:03 +0800

    change naming for release option
    move snapshot id forward
    
    Change-Id: I537c938c562dd4933e422698bdc84b94cacb8fb5
    Signed-off-by: Gui Chen <gui.chen@intel.com>

--- mic/imager/baseimager.py
+++ mic/imager/baseimager.py
@@ -100,7 +100,7 @@ class BaseImageCreator(object):
             self.destdir = os.path.abspath(os.path.expanduser(self.destdir))
 
             if 'release' in createopts and createopts['release']:
-                self.name += '-' + createopts['release']
+                self.name = createopts['release'] + '_' + self.name
 
             if self.pack_to:
                 if '@NAME@' in self.pack_to:
